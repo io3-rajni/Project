917c5f5cda13777f9047ad8abfabded40e27f076
Rajni <io3.rajni@gmail.com>
2023-09-01 23:55:55 +0530

    modified

--- project/src/Component/ProfileDialog.js
+++ project/src/Component/ProfileDialog.js
@@ -7,37 +7,102 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
 
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
 const ProfileDialog = (props) => {
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [number, setNumber] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [state, setState] = React.useState("");
+  const [address, setAddress] = React.useState("");
+  const [district, setDistrict] = React.useState("");
+  const [submit, setSubmit] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    number: "",
+    address: "",
+    state: "",
+    district: "",
+  });
   const { openProfileDialog, setOpenProfileDialog } = props;
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
 
-  // const handleClickOpen = () => {
-  //   setOpenProfileDialog(true);
-  // };
-
-  const handleClose = () => {
-    setOpenProfileDialog(false);
+  const handleFirstName = (e) => {
+    setFirstName(e?.target?.value);
+    console.log("first name", firstName);
+  };
+  const handleLastName = (e) => {
+    setLastName(e?.target?.value);
+    console.log("Last Name", lastName);
+  };
+  const handleAddress = (e) => {
+    setAddress(e?.target?.value);
+    console.log("Address", address);
+  };
+  const handleEmail = (e) => {
+    setEmail(e?.target?.value);
+    console.lob("Email", email);
+  };
+  const handlePassword = (e) => {
+    setPassword(e?.target?.value);
+    console.log("Password", password);
+  };
+  const handleState = (e) => {
+    setState(e?.target?.value);
+    console.log("State", state);
+  };
+  const handleDistrict = (e) => {
+    setDistrict(e?.target?.value);
+    console.log("District", district);
+  };
+  const handleNumber = (e) => {
+    setNumber(e?.target?.value);
+    console.log("Number", number);
   };
 
+  const handleSubmit = () => {
+    setSubmit({
+      firstName: firstName,
+      lastName: lastName,
+      number: number,
+      email: email,
+      password: password,
+      address: address,
+      state: state,
+      district: district,
+    });
+  };
+  const handleClear = () => {
+    firstName("");
+    lastName("");
+
+    setOpenProfileDialog(false);
+  };
   return (
     <>
-      {/* <Button variant="outlined" onClick={handleClickOpen}>
-        Open responsive dialog
-      </Button> */}
       <Dialog
         fullScreen={fullScreen}
         open={openProfileDialog}
-        // onClose={handleClose}
         aria-labelledby="responsive-dialog-title"
       >
         <DialogTitle id="responsive-dialog-title">
           {"Create your Profile"}
         </DialogTitle>
+        <Stack
+          direction="row"
+          sx={{ display: "flex", justifyContent: "center" }}
+        >
+          <Avatar src="/broken-image.jpg" />
+        </Stack>
         <DialogContent>
           <Box
             component="form"
@@ -52,57 +117,74 @@ const ProfileDialog = (props) => {
               label="First Name"
               type="text"
               variant="standard"
+              onChange={handleFirstName}
+              value={firstName}
             />
             <TextField
               id="standard-lastName-input"
               label="Last Name"
               type="text"
               variant="standard"
+              onChange={handleLastName}
+              value={lastName}
             />
             <TextField
               id="standard-email-input"
               label="Email"
               type="text"
               variant="standard"
+              onChange={handleEmail}
+              value={email}
             />
             <TextField
               id="standard-password-input"
               label="Password"
               type="password"
               variant="standard"
+              onChange={handlePassword}
+              value={password}
             />
             <TextField
               id="standard-Number-input"
               label="Phone Number"
               type="number"
               variant="standard"
+              onChange={handleNumber}
+              value={number}
             />
             <TextField
               id="standard-Address-input"
               label="Address"
               type="text"
               variant="standard"
+              onChange={handleAddress}
+              value={address}
             />
             <TextField
               id="standard-State-input"
               label="State"
               type="text"
               variant="standard"
+              onc
+              onChange={handleState}
+              value={state}
             />
             <TextField
               id="standard-district-input"
               label="District"
               type="text"
               variant="standard"
+              onChange={handleDistrict}
+              value={district}
             />
           </Box>
         </DialogContent>
         <DialogActions>
-          <Button autoFocus onClick={handleClose}>
-            Disagree
+          <Button autoFocus onClick={handleClear}>
+            clear
           </Button>
-          <Button onClick={handleClose} autoFocus>
-            Agree
+          <Button onClick={handleSubmit} autoFocus>
+            submit
           </Button>
         </DialogActions>
       </Dialog>
